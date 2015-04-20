a5a6b0408f3dc00a1c94612bc5559ef000d79735
Thomas Smith <t.n.smith@student.tudelft.nl>
2015-04-20 16:18:01 +0200

    Added checkstyle.xml. Please use this config file./rm-history.sh  Added dependencies: jUnit, Mockito and Hamcrest. Also added maven plugins: checkstyle, pmd, cobertura, findbugs

--- src/main/java/main.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Martijn on 20-4-2015.
- */
-public class main {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/main/java/nl/tudelft/ti2806/main/package-info.java
@@ -0,0 +1,4 @@
+/**full
+ * The main package of the application, launch related stuff goed here.
+ */
+package nl.tudelft.ti2806.main;
