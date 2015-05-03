0897de1a847864a5fbfdbec647b6f09453746337
Thomas Smith <t.n.smith@student.tudelft.nl>
2015-05-03 18:17:45 +0200

    Removed package-info files as they give errors in Eclispe. We'll use package.html files later on.

--- desktop/src/test/nl/tudelft/ti2806/monkeyrush/desktop/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * All tests for desktop.
- */
-package nl.tudelft.ti2806.monkeyrush.desktop;
--- html/src/test/nl/tudelft/ti2806/monkeyrush/client/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Tests for the HTML clients.
- */
-package nl.tudelft.ti2806.monkeyrush.client;
