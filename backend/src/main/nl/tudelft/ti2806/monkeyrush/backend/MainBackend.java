57574bd30771c07dc988c5b58fddb548d148f7ee
Thomas Smith <t.n.smith@student.tudelft.nl>
2015-05-04 10:35:07 +0200

    Added source file to backend to keep FindBugs from crashing.

---
+++ backend/src/main/nl/tudelft/ti2806/monkeyrush/backend/MainBackend.java
@@ -0,0 +1,11 @@
+package nl.tudelft.ti2806.monkeyrush.backend;
+
+/**
+ * Created by thomas on 4-5-15.
+ */
+public class MainBackend {
+
+    public static void main(String[] args) {
+        System.out.println("This is the backend.");
+    }
+}
