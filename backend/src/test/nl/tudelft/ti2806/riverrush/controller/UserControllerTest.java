37088c1021eaf05859af993f05210e686df66a97
mitchellolsthoorn <mitchell.olsthoorn@gmail.com>
2015-06-03 10:04:13 +0200

    Fixed tests

--- backend/src/test/nl/tudelft/ti2806/riverrush/controller/UserControllerTest.java
+++ backend/src/test/nl/tudelft/ti2806/riverrush/controller/UserControllerTest.java
@@ -1,10 +1,12 @@
 package nl.tudelft.ti2806.riverrush.controller;
 
 import nl.tudelft.ti2806.riverrush.domain.event.AnimalAddedEvent;
+import nl.tudelft.ti2806.riverrush.game.Game;
 import org.junit.Test;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.same;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 /**
@@ -15,7 +17,7 @@ public class UserControllerTest extends ControllerTest {
     @Override
     public void setup() {
         super.setup();
-        controller = new UserController(this.dispatcherMock, this.serverMock);
+        controller = new UserController(this.dispatcherMock, this.serverMock, mock(Game.class));
     }
 
     /**
