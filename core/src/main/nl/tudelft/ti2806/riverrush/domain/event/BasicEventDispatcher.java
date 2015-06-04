40e335b61a0eebb9f52c07bee7386d1ad0497759
Thomas Smith <t.n.smith@student.tudelft.nl>
2015-06-04 11:28:51 +0200

    Added log4j 2 to the project and implemented logging for EventDispatcher.

--- core/src/main/nl/tudelft/ti2806/riverrush/domain/event/BasicEventDispatcher.java
+++ core/src/main/nl/tudelft/ti2806/riverrush/domain/event/BasicEventDispatcher.java
@@ -1,6 +1,8 @@
 package nl.tudelft.ti2806.riverrush.domain.event;
 
 import com.google.inject.Singleton;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Hashtable;
 import java.util.LinkedList;
@@ -15,6 +17,7 @@ import java.util.Map;
 @Singleton
 public class BasicEventDispatcher implements EventDispatcher {
 
+    static final Logger log = LogManager.getLogger(BasicEventDispatcher.class);
     /**
      * Maps event types to a list of listeners.
      */
@@ -31,6 +34,7 @@ public class BasicEventDispatcher implements EventDispatcher {
 
         handlers.add(handler);
         this.registeredLambdas.put(eventType, handlers);
+        log.debug("Attached handler: " + handler + " for: " + eventType);
     }
 
     @Override
@@ -40,6 +44,9 @@ public class BasicEventDispatcher implements EventDispatcher {
 
         if (handlers != null) {
             handlers.remove(handlerLambda);
+            log.debug("Detached handler: " + handlerLambda + " for: " + eventType);
+        } else {
+            log.warn("Could not detach unregistered handler for: " + eventType);
         }
     }
 
@@ -60,7 +67,10 @@ public class BasicEventDispatcher implements EventDispatcher {
         List<HandlerLambda> handlers = this.registeredLambdas.get(event.getClass());
         if (handlers != null) {
             handlers.forEach(
-                (f) -> f.handle(event)
+                (f) -> {
+                    log.debug("Dispatching event: " + event.getClass().getSimpleName() + " to handler: " + f);
+                    f.handle(event);
+                }
             );
         }
     }
