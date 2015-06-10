22e56cf58392b9e65a424803f6c44d0f7e6547ee
JobEngel <job.engel@live.nl>
2015-06-10 10:53:48 +0200

    Changed game track

--- backend/src/main/nl/tudelft/ti2806/riverrush/game/BasicGameTrack.java
+++ backend/src/main/nl/tudelft/ti2806/riverrush/game/BasicGameTrack.java
@@ -14,7 +14,7 @@ public class BasicGameTrack extends GameTrack {
      * @param dispatcher - See {@link EventDispatcher}
      */
     public BasicGameTrack(final EventDispatcher dispatcher) {
-        super("--#-#---@--#--#-#--", dispatcher);
+        super("--[@2]-[@2]---[@8]--[@8]--[@2]-[@8]--", dispatcher);
         this.addTeam(new Team());
         this.addTeam(new Team());
 
