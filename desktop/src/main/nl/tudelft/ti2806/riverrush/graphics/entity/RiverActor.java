14cfe68e0ac6c907f8f06d15837f6762cf9121f5
JobEngel <job.engel@live.nl>
2015-06-14 15:24:52 +0200

    River now properly flows sideways on update flow as well.

--- desktop/src/main/nl/tudelft/ti2806/riverrush/graphics/entity/RiverActor.java
+++ desktop/src/main/nl/tudelft/ti2806/riverrush/graphics/entity/RiverActor.java
@@ -34,11 +34,11 @@ public class RiverActor extends Actor {
      */
     @Inject
     public RiverActor(final float startPosition, final float width, final float height) {
-        this.setPosition(width, 0);
+        this.setPosition(0, 0);
         this.setWidth(width);
         this.setHeight(height);
         this.originalPosition = startPosition;
-        this.mid = width / 2;
+        this.mid = height / 2;
 
         this.flow = new MoveToAction();
         this.flow.setPosition(width * -1, 0);
@@ -79,25 +79,24 @@ public class RiverActor extends Actor {
         float speedMultiplier = (float) speed;
         float currentFlow = Math.min(MAX_FLOW_DURATION, BASE_FLOW_DURATION / speedMultiplier);
         // speed <= 0.2 ? 15f : BASE_FLOW_DURATION / speedMultiplier;
-        float resetDuration = currentFlow * (1 - (this.getY() / (-1 * this.getHeight())));
+        float resetDuration = currentFlow * (1 - (this.getX() / (-1 * this.getWidth())));
 
         this.flow.setDuration(resetDuration);
 
         this.clearActions();
 
-        MoveToAction newDown = new MoveToAction();
-        newDown.setPosition(0, this.getHeight() * -1);
-        newDown.setDuration(currentFlow);
+        MoveToAction newFlow = new MoveToAction();
+        newFlow.setPosition(this.getWidth() * -1, 0);
+        newFlow.setDuration(currentFlow);
 
-        MoveToAction newUp = new MoveToAction();
-        newUp.setPosition(0, this.originalPosition);
+        MoveToAction newReset = new MoveToAction();
+        newReset.setPosition(this.originalPosition, 0);
 
-        SequenceAction newSeq = sequence(newUp, newDown);
+        SequenceAction newSeq = sequence(newReset, newFlow);
         RepeatAction newRep = forever(newSeq);
 
         SequenceAction seq = sequence(this.flow, newRep);
         this.addAction(seq);
-        this.flow.setTime(0f);
 
     }
 }
